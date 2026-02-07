eff37d7d69dbbacadf6f6545beff548858ac16a9
YoDobchev <yo.dobchev@gmail.com>
2026-02-07 23:13:13 +0200

    small not needed import

--- go-online-view/src/components/GameList.tsx
+++ go-online-view/src/components/GameList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { API_BASE } from "../config";
 import "../styles/gamelist.scss";
 import { useNavigate } from "react-router-dom";
