c1e44600523e598967b9e7b33574e8cb11eb605b
opossum-3 <konstantin.nikolov.yuriev@gmail.com>
2026-02-08 14:04:27 +0200

    Blogs available for everyone

--- go-online-view/src/pages/Blogs.tsx
+++ go-online-view/src/pages/Blogs.tsx
@@ -1,18 +1,7 @@
-import { useContext } from "react";
 import Navbar from "../components/Navbar";
-import { UserContext } from "../context/UserContext";
 import BlogList from "../components/BlogList";
 
 const Blogs: React.FC = () => {
-    const { user, loading } = useContext(UserContext);
-    if (loading) return <div>Loading profile...</div>;
-    if (!user) {
-        return (
-            <div className="must-login">
-                You must be logged in to view this page.
-            </div>
-        );
-    }
 
     return (
         <div>
